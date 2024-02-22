4b121c407880c4bd96c57e68f617fff1f3a3988a
SlavaKuzkinHackathon <jess.888@yandex.ru>
2024-02-22 17:36:51 +0600

    22.02-01

--- front/app/api/auth.ts
+++ front/app/api/auth.ts
@@ -1,14 +1,15 @@
 import { createEffect } from 'effector-next'
 import { toast } from 'react-toastify'
 import api from '../axiosClient'
-import { AxiosError } from 'axios'
+import { AxiosError, AxiosResponse } from 'axios'
 import { HTTPStatus } from '@/constans'
 import { ISignInFx, ISignUpFx, IUser } from '@/types/auth'
 import jwtDecode from 'jwt-decode'
 import { setAuth, setUser } from '@/context/user'
+import IAuthResponse from '@/types/IAuthResponse'
 
 export const singUpFx = createEffect(
-  async ({ url, name, password, email }: ISignUpFx) => {
+  async ({ url, name, password, email }: ISignUpFx): Promise<AxiosResponse<IAuthResponse>> => {
     const { data } = await api.post(url, { name, password, email })
     const userDataReg: IUser = await jwtDecode(data.accessToken)
 
@@ -16,10 +17,10 @@ export const singUpFx = createEffect(
 
     localStorage.setItem('auth_registration', JSON.stringify(data.accessToken))
 
-    if (data.warningMessage) {
+ /*    if (data.warningMessage) {
       toast.warning(data.warningMessage)
       return
-    }
+    } */
 
     toast.success('Регистрация прошла успешно!')
 
@@ -28,7 +29,7 @@ export const singUpFx = createEffect(
 )
 
 export const singInFx = createEffect(
-  async ({ url, email, password }: ISignInFx) => {
+  async ({ url, email, password }: ISignInFx): Promise<AxiosResponse<IAuthResponse>> => {
     const result = await api.post(url, { email, password })
 
     const userData: IUser = await jwtDecode(result.data.accessToken)
@@ -84,7 +85,7 @@ export const checkUserAuthFx = createEffect(async (token: string | any) => {
   }
 }) */
 
-export const logoutFx = createEffect(async (url: string) => {
+export const logoutFx = createEffect(async (url: string): Promise<void> => {
   try {
     await api.get(url)
     setAuth(false)
