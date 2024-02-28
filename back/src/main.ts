e218b79d51e302ed14dd683e7d3939fa58f7d576
SlavaKuzkinHackathon <jess.888@yandex.ru>
2024-02-28 14:10:07 +0300

    28.02-03

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 
-const PORT = process.env.PORT ?? 3001;
+const PORT = process.env.PORT ?? 5000;
 
 async function start() {
   const app = await NestFactory.create(AppModule);
